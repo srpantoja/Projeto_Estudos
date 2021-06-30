23e284d07845be15ffb644031e423d33ae3a7f00
Jean Igor de Queiroz Pantoja <jeanpantoja@great.local.ufc.br>
2021-06-30 20:00:54 -0300

    uploading bug fixes

--- app/routes/client.js
+++ app/routes/client.js
@@ -2,8 +2,8 @@ let controller = require("../controllers/client.js")
 
 module.exports = (app) => {
 
-    //app.get("/api/client", controller.getAllClient)
-    //app.get("/api/client/:id", controller.getClient)
+    app.get("/api/client", controller.getAllclient)
+    app.get("/api/client/:id", controller.getclient)
     app.post("/api/client", controller.addclient)
-    //app.delete("/api/client/:id", controller.deleteClient)
+    app.delete("/api/client/:id", controller.deleteclient)
 }
